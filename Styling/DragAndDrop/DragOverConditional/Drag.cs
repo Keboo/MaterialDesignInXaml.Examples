f0133a6e23a16dbe83ae1697f0760d23f21136ab
Kevin B <kitokeboo@gmail.com>
2017-09-19 10:24:14 -0700

    Updating MDIX on all projects. Adding new DragOverCondition example

---
+++ Styling/DragAndDrop/DragOverConditional/Drag.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DragOverConditional
+{
+    public static class Drag
+    {
+        public static readonly DependencyProperty IsDragOverProperty = DependencyProperty.RegisterAttached(
+            "IsDragOver", typeof(bool), typeof(Drag), new PropertyMetadata(default(bool)));
+
+        public static void SetIsDragOver(DependencyObject element, bool value)
+        {
+            element.SetValue(IsDragOverProperty, value);
+        }
+
+        public static bool GetIsDragOver(DependencyObject element)
+        {
+            return (bool) element.GetValue(IsDragOverProperty);
+        }
+    }
+}
---
+++ Styling/DragAndDrop/DragOverConditional/DragOverBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace DragOverConditional
+{
+    public class DragOverBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.DragEnter += AssociatedObjectOnDragOver;
+            AssociatedObject.DragLeave += AssociatedObjectOnDragOff;
+            AssociatedObject.Drop += AssociatedObjectOnDragOff;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.DragEnter -= AssociatedObjectOnDragOver;
+            AssociatedObject.DragLeave -= AssociatedObjectOnDragOff;
+            AssociatedObject.Drop -= AssociatedObjectOnDragOff;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnDragOver(object sender, DragEventArgs dragEventArgs)
+        {
+            //TODO: Any sort of filtering that you might want to do
+            //Consider using the methods dragEventArgs.Data to check for particular types of data
+            Drag.SetIsDragOver(AssociatedObject, true);
+        }
+
+        private void AssociatedObjectOnDragOff(object sender, DragEventArgs dragEventArgs)
+        {
+            Drag.SetIsDragOver(AssociatedObject, false);
+        }
+    }
+}
