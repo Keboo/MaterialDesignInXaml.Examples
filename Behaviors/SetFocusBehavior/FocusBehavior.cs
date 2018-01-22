025ffa67646419a6d0831548467b0f8762fd9b14
Kevin B <kitokeboo@gmail.com>
2018-01-22 10:23:33 -0800

    Adding focus behavior example

---
+++ Behaviors/SetFocusBehavior/FocusBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace SetFocusBehavior
+{
+    public class FocusBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObjectOnIsVisibleChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= AssociatedObjectOnIsVisibleChanged;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            AssociatedObject.Focus();
+        }
+    }
+}
