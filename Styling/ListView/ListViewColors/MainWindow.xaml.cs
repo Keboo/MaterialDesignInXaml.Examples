9fa75bcff0d61d1d6a3e16e8941e4f64fbe76bbf
Kevin B <kitokeboo@gmail.com>
2018-02-11 20:59:52 -0800

    Adding simple example of overriding colors and using a ListView with the Dark theme.

---
+++ Styling/ListView/ListViewColors/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ListViewColors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public List<User> Users { get; }
+
+        public MainWindow()
+        {
+            Users = new List<User>
+            {
+                new User
+                {
+                    Name = "John Doe",
+                    Email = "john@family.com"
+                },
+                new User
+                {
+                    Name = "Jane Doe",
+                    Email = "jane@family.com"
+                },
+                new User
+                {
+                    Name = "Other Guy",
+                    Email = "guy@family.com"
+                }
+            };
+            InitializeComponent();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
