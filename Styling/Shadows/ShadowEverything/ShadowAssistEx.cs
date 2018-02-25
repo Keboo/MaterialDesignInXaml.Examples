8ebaea1f1e4900565f5a21d66bca6bdbf78079ef
Kevin B <kitokeboo@gmail.com>
2018-02-25 14:56:07 -0800

    Adding example showing attached property for using shadows.

---
+++ Styling/Shadows/ShadowEverything/ShadowAssistEx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media.Effects;
+using MaterialDesignThemes.Wpf;
+using MaterialDesignThemes.Wpf.Converters;
+
+namespace ShadowEverything
+{
+    public static class ShadowAssistEx
+    {
+        public static readonly DependencyProperty ShadowDepthProperty = DependencyProperty.RegisterAttached(
+            "ShadowDepth", typeof(ShadowDepth), typeof(ShadowAssistEx), new PropertyMetadata(default(ShadowDepth), OnShadowDepthChanged));
+
+        private static void OnShadowDepthChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            if (dependencyObject is UIElement element)
+            {
+                element.Effect =
+                    ShadowConverter.Instance.Convert(e.NewValue, typeof(Effect), null, CultureInfo.CurrentUICulture) as Effect;
+            }
+        }
+
+        public static void SetShadowDepth(DependencyObject element, ShadowDepth value)
+        {
+            element.SetValue(ShadowDepthProperty, value);
+        }
+
+        public static ShadowDepth GetShadowDepth(DependencyObject element)
+        {
+            return (ShadowDepth) element.GetValue(ShadowDepthProperty);
+        }
+    }
+}
