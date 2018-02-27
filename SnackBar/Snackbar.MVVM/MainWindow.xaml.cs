ac1725fa164c93eef27c9c3da5df05d0b46982eb
Kevin B <kitokeboo@gmail.com>
2018-02-26 22:45:41 -0800

    MVVM Example of using Snackbar.

---
+++ SnackBar/Snackbar.MVVM/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+
+namespace Snackbar.MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+
+            Messenger.Default.Register<ShowMessage>(this, OnShowMessage);
+        }
+
+        private void OnShowMessage(ShowMessage message)
+        {
+            Snackbar.MessageQueue.Enqueue(message.Message);
+        }
+    }
+}
