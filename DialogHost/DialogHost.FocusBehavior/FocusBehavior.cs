b98cc8c950dac6f0be49550a645d55c1254ca2da
Kevin B <kitokeboo@gmail.com>
2018-02-28 21:55:58 -0800

    Adding example of focus behavior for dialog hosts.

---
+++ DialogHost/DialogHost.FocusBehavior/FocusBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace DialogHost.FocusBehavior
+{
+    public class FocusBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObjectOnIsVisibleChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= AssociatedObjectOnIsVisibleChanged;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            AssociatedObject.Focus();
+        }
+    }
+}
