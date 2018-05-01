fd0371886ddc5c8342e43725846abaa134647789
Marc Vaccaro <marcgvaccaro@gmail.com>
2018-05-01 13:31:36 -0700

    Add DialogHost code-behind examples

---
+++ DialogHost.CodeBehindExample/Dialogs/TextInputDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogHost.CodeBehindExample.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for TextInputDialog.xaml
+    /// </summary>
+    public partial class TextInputDialog : UserControl
+    {
+        public TextInputDialog(TextInputDialogViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+
+    public class TextInputDialogViewModel : ViewModelBase
+    {
+        public string Message { get; set; } = "Enter some text";
+        public string AffirmativeButtonText { get; set; } = "OK";
+        public string NegativeButtonText { get; set; } = "CANCEL";
+
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged(); }
+        }
+    }
+}
---
+++ DialogHost.CodeBehindExample/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using DialogHost.CodeBehindExample.Dialogs;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogHost.CodeBehindExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Snackbar.MessageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(1000));
+        }
+
+        private async void GroupAButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OkCancelDialog();
+            var result = (bool)await MaterialDesignThemes.Wpf.DialogHost.Show(dialog, "GroupDialogHost");
+
+            Snackbar.MessageQueue.Enqueue($"{(result ? "OK" : "CANCEL")} Button was clicked");
+        }
+
+        private async void GroupBButtonClick(object sender, RoutedEventArgs e)
+        {
+            var viewModel = new TextInputDialogViewModel
+            {
+                Message = "Enter some text"
+            };
+
+            var dialog = new TextInputDialog(viewModel);
+            var result = (bool)await MaterialDesignThemes.Wpf.DialogHost.Show(dialog, "MainDialogHost");
+            if (result)
+            {
+                Snackbar.MessageQueue.Enqueue($"Received text: {viewModel.Text}");
+            }
+        }
+
+        private async void GroupCButtonClick(object sender, RoutedEventArgs e)
+        {
+            var viewModel = new TextInputDialogViewModel
+            {
+                AffirmativeButtonText = "TRY NOW",
+                NegativeButtonText = "NEVERMIND",
+                Message = "Type 'cool' into the box"
+            };
+
+            var dialog = new TextInputDialog(viewModel);
+            await MaterialDesignThemes.Wpf.DialogHost.Show(dialog, "MainDialogHost", DialogClosing);
+            Snackbar.MessageQueue.Enqueue($"We did it!");
+        }
+
+        private void DialogClosing(object sender, DialogClosingEventArgs e)
+        {
+            if (!(bool)e.Parameter)
+            {
+                //cancel button was clicked, don't do any of this logic
+                return;
+            }
+
+            //This will get called when the dialog is about to close
+            if (e.Session.Content is TextInputDialog dialog)
+            {
+                if (dialog.DataContext is TextInputDialogViewModel viewModel)
+                {
+                    if (viewModel.Text?.ToLower() != "cool")
+                    {
+                        viewModel.Text = "NO!";
+                        e.Cancel(); //prevents the dialog from closing
+                    }
+                }
+            }
+        }
+    }
+}
