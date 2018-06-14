80809e652b3d1772008ed6a65c1db5981ee9070b
Kevin B <kitokeboo@gmail.com>
2018-06-14 14:59:37 -0700

    Adding example of changing the validation background color.

---
+++ Styling/Validation/ValidationBackgroundChange/SimpleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using GalaSoft.MvvmLight;
+
+namespace ValidationBackgroundChange
+{
+    public class SimpleViewModel : AnnotationValidationViewModel
+    {
+        private string _FirstName;
+        [Required]
+        public string FirstName
+        {
+            get => _FirstName;
+            set => Set(ref _FirstName, value);
+        }
+
+        private string _LastName;
+        [Required]
+        public string LastName
+        {
+            get => _LastName;
+            set => Set(ref _LastName, value);
+        }
+        
+        public SimpleViewModel()
+        {
+            //This will force validation to show initially
+            ValidateModel();
+        }
+    }
+}
