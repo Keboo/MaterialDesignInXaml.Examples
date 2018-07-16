b758103fb109aaa8ac29df9c6f89449e05d66774
Kevin B <kitokeboo@gmail.com>
2018-07-16 11:07:58 -0700

    Adding TemplateSelector example. Updating nugets.

---
+++ MVVM/MVVM.TemplateSelector/MyTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MVVM.TemplateSelector
+{
+    [ContentProperty(nameof(Templates))]
+    public class MyTemplateSelector : DataTemplateSelector
+    {
+        public ResourceDictionary Templates { get; set; } = new ResourceDictionary();
+
+
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Option option)
+            {
+                return Templates[option.Type] as DataTemplate;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
