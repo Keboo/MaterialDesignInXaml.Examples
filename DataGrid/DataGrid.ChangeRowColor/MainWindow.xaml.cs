bc32db26615946d2d4d0764c1a0ecba70cf01b82
Kevin B <kitokeboo@gmail.com>
2018-07-30 00:10:14 -0700

    Addin example for changing data rid's row color. Updating all projects to 2.4.1 release.

---
+++ DataGrid/DataGrid.ChangeRowColor/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Bogus;
+
+namespace DataGrid.ChangeRowColor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Person> People { get; }
+
+        public MainWindow()
+        {
+            Faker<Person> generator = new Faker<Person>()
+                .StrictMode(true)
+                .RuleFor(x => x.ID, f => f.IndexGlobal)
+                .RuleFor(x => x.FirstName, f => f.Person.FirstName)
+                .RuleFor(x => x.LastName, f => f.Person.LastName)
+                .RuleFor(x => x.DOB, f => f.Person.DateOfBirth);
+
+            People = generator.Generate(300);
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataGrid/DataGrid.ChangeRowColor/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataGrid.ChangeRowColor
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
