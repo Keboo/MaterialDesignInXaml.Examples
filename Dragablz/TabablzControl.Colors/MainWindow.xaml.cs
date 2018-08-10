04ac00cd24b9b697f6ce14acfeede24072844c12
Kevin B <kitokeboo@gmail.com>
2018-08-10 09:00:44 -0700

    Adding example showing web browser in dialog host failure. Adding example of Dragablz colors.

---
+++ Dragablz/TabablzControl.Colors/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TabablzControl.Colors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
