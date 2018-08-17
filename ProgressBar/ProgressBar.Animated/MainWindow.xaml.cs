8e70754a0621561b45b63676f80a1e4513043093
Kevin B <kitokeboo@gmail.com>
2018-08-16 21:23:56 -0700

    Adding example of progress bar animating between its value changes.

---
+++ ProgressBar/ProgressBar.Animated/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace ProgressBar.Animated
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PreviousOnClick(object sender, RoutedEventArgs e)
+        {
+            if (AnimateBehavior.IsAnimating) return;
+            ProgressBar.Value = Math.Max(ProgressBar.Minimum, ProgressBar.Value - 25);
+        }
+
+        private void NextOnClick(object sender, RoutedEventArgs e)
+        {
+            if (AnimateBehavior.IsAnimating) return;
+            ProgressBar.Value = Math.Min(ProgressBar.Maximum, ProgressBar.Value + 25);
+        }
+    }
+}
---
+++ ProgressBar/ProgressBar.Animated/ProgresBarAnimateBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+using System.Windows.Media.Animation;
+
+namespace ProgressBar.Animated
+{
+    //Based on https://stackoverflow.com/questions/26618308/wpf-smoothly-animated-progressbar-in-template
+    public class ProgresBarAnimateBehavior : Behavior<System.Windows.Controls.ProgressBar>
+    {
+        private bool _IsAnimating;
+
+        public bool IsAnimating => _IsAnimating;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            System.Windows.Controls.ProgressBar progressBar = AssociatedObject;
+            progressBar.ValueChanged += ProgressBar_ValueChanged;
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_IsAnimating)
+                return;
+
+            _IsAnimating = true;
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation
+                (e.OldValue, e.NewValue, new Duration(TimeSpan.FromSeconds(0.3)), FillBehavior.Stop);
+            doubleAnimation.Completed += AnimationCompleted;
+
+            ((System.Windows.Controls.ProgressBar)sender).BeginAnimation(RangeBase.ValueProperty, doubleAnimation);
+
+            e.Handled = true;
+
+            void AnimationCompleted(object _, EventArgs __)
+            {
+                _IsAnimating = false;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.ValueChanged -= ProgressBar_ValueChanged;
+            base.OnDetaching();
+        }
+    }
+}
