e069e8b941835586780d687265322b503efb013f
Kevin B <kitokeboo@gmail.com>
2018-10-23 22:23:49 -0700

    Adding simple example for saving the theme

---
+++ Theming/PersistThemePreferences/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MaterialDesignThemes.Wpf;
+using PersistThemePreferences.Properties;
+using System;
+using System.Windows;
+
+namespace PersistThemePreferences
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetTheme(Settings.Default.Theme);
+            base.OnStartup(e);
+        }
+
+        public void SetTheme(BaseTheme theme)
+        {
+            //Copied from the existing ThemeAssist class
+            //https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/blob/master/MaterialDesignThemes.Wpf/ThemeAssist.cs
+
+            string lightSource =  "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml";
+            string darkSource = "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml";
+
+            foreach (ResourceDictionary resourceDictionary in Resources.MergedDictionaries)
+            {
+                if (string.Equals(resourceDictionary.Source?.ToString(), lightSource, StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(resourceDictionary.Source?.ToString(), darkSource, StringComparison.OrdinalIgnoreCase))
+                {
+                    Resources.MergedDictionaries.Remove(resourceDictionary);
+                    break;
+                }
+            }
+
+            if (theme == BaseTheme.Dark)
+            {
+                Resources.MergedDictionaries.Insert(0, new ResourceDictionary { Source = new Uri(darkSource) });
+            }
+            else
+            {
+                //This handles both Light and Inherit
+                Resources.MergedDictionaries.Insert(0, new ResourceDictionary { Source = new Uri(lightSource) });
+            }
+        }
+    }
+}
---
+++ Theming/PersistThemePreferences/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PersistThemePreferences.Properties;
+using System.Windows;
+
+namespace PersistThemePreferences
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Toggle.IsChecked = Settings.Default.Theme == MaterialDesignThemes.Wpf.BaseTheme.Dark;
+            SetTheme();
+        }
+
+        private void Toggle_Checked(object sender, RoutedEventArgs e) => SetTheme();
+
+        private void Toggle_Unchecked(object sender, RoutedEventArgs e) => SetTheme();
+
+        private void SetTheme()
+        {
+            if (Toggle.IsChecked == true)
+            {
+                Text.Text = "Dark Theme";
+                Settings.Default.Theme = MaterialDesignThemes.Wpf.BaseTheme.Dark;
+            }
+            else
+            {
+                Text.Text = "Light Theme";
+                Settings.Default.Theme = MaterialDesignThemes.Wpf.BaseTheme.Light;
+            }
+
+            Settings.Default.Save();
+            ((App)Application.Current).SetTheme(Settings.Default.Theme);
+        }
+    }
+}
