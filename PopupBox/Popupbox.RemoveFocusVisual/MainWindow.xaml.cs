ce8ff10c8f14058ee913811cf81b85fb3a5d8d92
Kevin Bost <kevin@intellitect.com>
2018-12-10 11:42:16 -0800

    New example removing the focus visual style from popup box.

---
+++ PopupBox/Popupbox.RemoveFocusVisual/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Popupbox.RemoveFocusVisual
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
