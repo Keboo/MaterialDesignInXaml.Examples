f5bfc7938a70de7eb99a835a24946a85135ccb0d
Kevin Bost <kevin@intellitect.com>
2019-01-29 09:28:15 -0800

    Adding example locking the drawer host open.

---
+++ DrawerHost/DrawerHost.PreventClose/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DrawerHost.PreventClose
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _DrawerOpen;
+        public bool DrawerOpen
+        {
+            get => _DrawerOpen;
+            set
+            {
+                if (!value && LockDrawer)
+                {
+                    //If the drawer is locked open, prevent the change
+                    return;
+                }
+                Set(ref _DrawerOpen, value);
+            }
+        }
+
+        private bool _LockDrawer;
+        public bool LockDrawer
+        {
+            get => _LockDrawer;
+            set => Set(ref _LockDrawer, value);
+        }
+
+        private void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
