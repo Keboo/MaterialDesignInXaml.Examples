a5c1fa735244f1f603eeeb1f6c598aa4292e119b
Kevin Bost <kevin@intellitect.com>
2019-02-27 08:54:00 -0800

    Adding example of hyjacking the routed command to prevent the dialog from closing.

---
+++ DialogHost/DialogHost.RoutedCommand/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace DialogHost.RoutedCommand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_OnCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.Handled = true;
+            e.CanExecute = CheckBox.IsChecked == true;
+        }
+    }
+}
