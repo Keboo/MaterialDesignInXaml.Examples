73c88723fded0e09faed4d3c0b68d3aa49697fe6
Kevin Bost <kitokeboo@gmail.com>
2019-03-21 21:18:26 -0700

    Adding example of using MahApps validation template.

---
+++ MahApps/ValidationTemplate/ValidationTemplate/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ValidationTemplate.Annotations;
+
+namespace ValidationTemplate
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _NotEmpty;
+        public string NotEmpty
+        {
+            get => _NotEmpty;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Can't be empty");
+                }
+                if (_NotEmpty != value)
+                {
+                    _NotEmpty = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
