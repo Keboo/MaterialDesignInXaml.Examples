dfa6378ed33349cd743463fb128b50f45c1e082a
Kevin Bost <kevin@intellitect.com>
2019-03-26 21:41:01 -0700

    Adding data bound transitioner example.

---
+++ Transitioner/Transitioner.DataBoundSlides/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Transitioner.DataBoundSlides
+{
+    public class MainWindowViewModel
+    {
+        public IList<int> Slides { get; } = new List<int>
+        {
+            1, 2, 3, 4, 5, 6, 7
+        };
+
+        public int SelectedIndex { get; set; }
+    }
+}
