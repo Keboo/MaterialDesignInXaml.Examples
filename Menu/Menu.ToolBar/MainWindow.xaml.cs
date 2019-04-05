d3ea38f8a0a140ca05590e0cd5070dcb668ec58e
Kevin Bost <kevin@intellitect.com>
2019-04-05 12:32:13 -0700

    Adding menu styled like a toolbar as an example.

---
+++ Menu/Menu.ToolBar/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace Menu.ToolBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public static RoutedCommand MenuItemSelected { get; } = new RoutedCommand("MenuItemSelected", typeof(MainWindow));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SelectedMenuTextBlock.Text = $"Selected Item: {e.Parameter}";
+        }
+    }
+}
