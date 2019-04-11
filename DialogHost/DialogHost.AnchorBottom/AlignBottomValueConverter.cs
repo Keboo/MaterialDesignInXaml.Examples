1630ec5191acbea7a05411845a2fb9d9aab9677c
Kevin Bost <kitokeboo@gmail.com>
2019-04-11 13:21:17 -0700

    Adding example of anchoring a dialog to the bottom

---
+++ DialogHost/DialogHost.AnchorBottom/AlignBottomValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DialogHost.AnchorBottom
+{
+
+    public class AlignBottomValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values?.Length >= 2 &&
+                values[0] is double dialogHostHeight &&
+                values[1] is double dialogHeight &&
+                IsValid(dialogHostHeight) &&
+                IsValid(dialogHeight))
+            {
+                //Dialog will be centered
+                return (dialogHostHeight - dialogHeight) / 2;
+            }
+            return Binding.DoNothing;
+
+            bool IsValid(double value) => !double.IsInfinity(value) && !double.IsNaN(value);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
