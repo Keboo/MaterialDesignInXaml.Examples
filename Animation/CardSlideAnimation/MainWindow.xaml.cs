a030b267d62f54c16ad004dba88cd910661d0acc
Kevin Bost <kitokeboo@gmail.com>
2019-05-16 22:56:21 -0700

    Adding animations for CardSlideAnimation

--- Animation/CardSlideAnimation/MainWindow.xaml.cs
+++ Animation/CardSlideAnimation/MainWindow.xaml.cs
@@ -1,28 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace CardSlideAnimation
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly Storyboard _storyboard;
         public MainWindow()
         {
             InitializeComponent();
+
+            Storyboard sb = new Storyboard();
+            sb.FillBehavior = FillBehavior.HoldEnd;
+
+            var opacityAnimation = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromMilliseconds(200)));
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath(nameof(Card.Opacity)));
+
+            sb.Children.Add(opacityAnimation);
+
+            var translateAnimation = new DoubleAnimation(80, 0, new Duration(TimeSpan.FromMilliseconds(300)));
+            Storyboard.SetTargetProperty(translateAnimation, new PropertyPath($"{nameof(Card.RenderTransform)}.{nameof(TranslateTransform.X)}"));
+
+            sb.Children.Add(translateAnimation);
+
+            _storyboard = sb;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _storyboard.Begin(Card);
         }
     }
 }
