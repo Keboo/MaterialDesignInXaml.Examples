2356f3bb1dac907d41816a26dd81804a8b4295ab
Kevin Bost <kevin@intellitect.com>
2019-05-29 07:52:51 -0700

    Adding attached properties to show/hide minimize and maximize buttons. Fixes #10

---
+++ Window/MDIXWindow/WindowEx.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace MDIXWindow
+{
+    public static class WindowEx
+    {
+        public static readonly DependencyProperty ShowMinimizeButtonProperty = DependencyProperty.RegisterAttached(
+            "ShowMinimizeButton", typeof(bool), typeof(WindowEx), new PropertyMetadata(true));
+
+        public static void SetShowMinimizeButton(DependencyObject element, bool value)
+        {
+            element.SetValue(ShowMinimizeButtonProperty, value);
+        }
+
+        public static bool GetShowMinimizeButton(DependencyObject element)
+        {
+            return (bool) element.GetValue(ShowMinimizeButtonProperty);
+        }
+
+        public static readonly DependencyProperty ShowMaximizeButtonProperty = DependencyProperty.RegisterAttached(
+            "ShowMaximizeButton", typeof(bool), typeof(WindowEx), new PropertyMetadata(true));
+
+        public static void SetShowMaximizeButton(DependencyObject element, bool value)
+        {
+            element.SetValue(ShowMaximizeButtonProperty, value);
+        }
+
+        public static bool GetShowMaximizeButton(DependencyObject element)
+        {
+            return (bool) element.GetValue(ShowMaximizeButtonProperty);
+        }
+    }
+}
