d520c6f9b80ca807e58e915d76a7e66a068f5933
Kevin Bost <kevin@intellitect.com>
2019-05-29 21:29:22 -0700

    Adding GridView colors example

---
+++ ListView/ListView.GridViewColors/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TestData;
+
+namespace ListView.GridViewColors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public IList<Person> People { get; }
+
+        public MainWindow()
+        {
+            People = Data.GeneratePeople(150);
+
+            InitializeComponent();
+        }
+    }
+}
