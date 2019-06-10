d6a3ce8c92dcc1a33a652b485f1f5efbf1b5a642
Kevin Bost <kitokeboo@gmail.com>
2019-06-09 23:08:14 -0700

    Adding example of using a PackIcon for a Window icon.

---
+++ Window/Window.Icon/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Window.Icon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Window/Window.Icon/VisualToImageSourceConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Window.Icon
+{
+    [ValueConversion(typeof(FrameworkElement), typeof(ImageSource))]
+    public class VisualToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is FrameworkElement element)
+            {
+                element.Measure(new Size(element.Width, element.Height));
+                element.Arrange(new Rect(new Size(element.Width, element.Height)));
+                var rtb = new RenderTargetBitmap((int)Math.Ceiling(element.ActualWidth), 
+                    (int)Math.Ceiling(element.ActualHeight), 96, 96, PixelFormats.Pbgra32);
+                rtb.Render(element);
+
+                return rtb;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
