cfdf27cb26387ef5e3fa86d2d7f94f57f181592d
Kevin Bost <kitokeboo@gmail.com>
2019-06-27 09:39:47 -0700

    An example layout using both a DialogHost and a Snackbar that is shown on top of it.

---
+++ SnackBar/Snackbar.OnTopOfDialog/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Snackbar.OnTopOfDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMessage_OnClick(object sender, RoutedEventArgs e)
+        {
+            Snackbar.MessageQueue.Enqueue("Snackbar Message!");
+        }
+    }
+}
