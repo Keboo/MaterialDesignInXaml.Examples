9364024cf70ec174889c8b788e164b85c9ddfbd5
Kevin Bost <kitokeboo@gmail.com>
2019-08-03 21:45:26 -0700

    Adding example of a single click tree view item for toggling expanded state

---
+++ TreeView/TreeView.SingleClickExpand/SingleClickTreeView.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TreeView.SingleClickExpand
+{
+    public class SingleClickTreeView : System.Windows.Controls.TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new SingleClickTreeViewItem();
+        }
+    }
+}
---
+++ TreeView/TreeView.SingleClickExpand/SingleClickTreeViewItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TreeView.SingleClickExpand
+{
+    public class SingleClickTreeViewItem : TreeViewItem
+    {
+        static SingleClickTreeViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SingleClickTreeViewItem), new FrameworkPropertyMetadata(typeof(SingleClickTreeViewItem)));
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            bool handled = e.Handled;
+            base.OnMouseLeftButtonDown(e);
+
+            if (!handled && IsEnabled)
+            {
+                if (e.ClickCount == 1)
+                {
+                    SetCurrentValue(IsExpandedProperty, !IsExpanded);
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
