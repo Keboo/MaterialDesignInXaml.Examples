23662528c276cad15bca8a22b0ca495e1b019a38
Kevin Bost <kitokeboo@gmail.com>
2019-10-24 21:32:20 -0700

    Adding example of using the attached property for the closing callback

---
+++ DialogHost/DialogHost.AttachedPropertyClosing/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MaterialDesignThemes.Wpf;
+
+namespace DialogHost.AttachedPropertyClosing
+{
+    public class MainWindowViewModel
+    {
+        public DialogClosingEventHandler DialogClosingHandler { get; }
+
+        public MainWindowViewModel()
+        {
+            DialogClosingHandler = OnDialogClosing;
+        }
+
+        private void OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            //TODO: Handle closing
+        }
+    }
+}
