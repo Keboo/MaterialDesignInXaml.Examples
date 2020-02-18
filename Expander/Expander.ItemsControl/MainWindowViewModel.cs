c3d99496d8f15d794d9798bcc27d734e3152d5b9
Kevin Bost <kitokeboo@gmail.com>
2020-02-18 10:26:26 -0800

    Adding expander in items control example.

---
+++ Expander/Expander.ItemsControl/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TestData;
+
+namespace Expander.ItemsControl
+{
+    public class MainWindowViewModel
+    {
+        public IList<Person> People { get; } = Data.GeneratePeople(10);
+
+    }
+}
