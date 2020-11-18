c5f17c4a390c2194c344f4dd5b05687993e9fd85
Kevin Bost <kitokeboo@gmail.com>
2020-11-17 17:30:55 -0800

    Adding sample of multi-select using a ListBox

---
+++ ListBox/ListBox.MultiSelectItems/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ListBox.MultiSelectItems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Delete_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var vm = (MainWindowViewModel)DataContext;
+            vm.DeleteCommand.Execute(e.Parameter);
+        }
+    }
+}
---
+++ ListBox/ListBox.MultiSelectItems/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MaterialDesignThemes.Wpf;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace ListBox.MultiSelectItems
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Item> Items { get; } = new();
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            Items.Add(new Item { Name = "Breakfast" });
+            Items.Add(new Item { Name = "Lunch" });
+            Items.Add(new Item { Name = "Dinner" });
+
+            AddCommand = new RelayCommand(OnAdd);
+            DeleteCommand = new RelayCommand<Item>(OnDelete);
+        }
+
+        private void OnDelete(Item item)
+        {
+            Items.Remove(item);
+        }
+
+        private async void OnAdd()
+        {
+            if (await DialogHost.Show(new Item()) is Item item)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+    }
+}
