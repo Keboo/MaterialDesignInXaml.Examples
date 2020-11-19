1e329a3db8f149dd1355fbec3f0da7f90fdb3065
Kevin Bost <kitokeboo@gmail.com>
2020-11-18 21:52:23 -0800

    Example showing extending and forwarding properties to a custom template

---
+++ Styling/ExtendTextBoxTemplate/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ExtendTextBoxTemplate
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public string _Text;
+        public string Text
+        {
+            get => _Text;
+            set
+            {
+                if (_Text != value)
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                    {
+                        throw new Exception("Must enter a value");
+                    }
+                    _Text = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Styling/ExtendTextBoxTemplate/TextBoxEx.cs
@@ -0,0 +1,25 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace ExtendTextBoxTemplate
+{
+    public static class TextBoxEx
+    {
+        public static PackIconKind GetIcon(DependencyObject obj)
+        {
+            return (PackIconKind)obj.GetValue(IconProperty);
+        }
+
+        public static void SetIcon(DependencyObject obj, PackIconKind value)
+        {
+            obj.SetValue(IconProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.RegisterAttached("Icon", typeof(PackIconKind), typeof(TextBoxEx), 
+                new PropertyMetadata(PackIconKind.None));
+
+
+    }
+}
