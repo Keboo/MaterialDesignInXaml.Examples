8ed04a72039fe20dda650461f7097c05ce8ed4e2
Kevin Bost <kitokeboo@gmail.com>
2022-06-09 21:25:54 -0700

    Adding in MaskedTextBoxBehavior example

---
+++ TextBox/TextBox.MaskedTextBox/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TextBox.MaskedTextBox;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
