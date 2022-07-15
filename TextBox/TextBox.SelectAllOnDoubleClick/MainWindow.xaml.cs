e83d92e5ed6ebc2083fda34044572b129a12c556
Kevin Bost <kitokeboo@gmail.com>
2022-07-15 09:37:37 -0700

    Adding select all on double click example

---
+++ TextBox/TextBox.SelectAllOnDoubleClick/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TextBox.SelectAllOnDoubleClick;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TextBox/TextBox.SelectAllOnDoubleClick/SelectAllBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Input;
+
+namespace TextBox.SelectAllOnDoubleClick;
+
+public class SelectAllBehavior : Behavior<System.Windows.Controls.TextBox>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.MouseDoubleClick += AssociatedObjectOnMouseDoubleClick;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.MouseDoubleClick -= AssociatedObjectOnMouseDoubleClick;
+        base.OnDetaching();
+    }
+
+    private void AssociatedObjectOnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        AssociatedObject.SelectAll();
+    }
+}
