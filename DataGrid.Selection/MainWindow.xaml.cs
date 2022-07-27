6253ffcf64a3e2f64bd03f7a53af76da56766814
Kevin Bost <kitokeboo@gmail.com>
2022-07-27 09:16:33 -0700

    Adding data grid selection example

---
+++ DataGrid.Selection/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DataGrid.Selection;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        DataContext = new MainWindowViewModel();
+        InitializeComponent();
+    }
+}
---
+++ DataGrid.Selection/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using TestData;
+
+namespace DataGrid.Selection;
+
+public class MainWindowViewModel : ObservableObject
+{
+    public ObservableCollection<Person> People { get; }
+
+    public MainWindowViewModel()
+    {
+        People = new(Data.GeneratePeople(100));
+    }
+
+    private Person? _selectedPerson;
+    public Person? SelectedPerson
+    {
+        get => _selectedPerson;
+        set => SetProperty(ref _selectedPerson, value);
+    }
+}
