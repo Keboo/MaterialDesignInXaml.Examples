cc6d629ea4feb5387fb7496a850e566e519e2be8
Kevin Bost <kitokeboo@gmail.com>
2023-02-11 00:56:31 -0800

    Adding example of using CommunityToolkit.Mvvm in a classlib

---
+++ MVVM/MVVM.Lib.Shared/SharedViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MVVM.Lib.Shared;
+
+
+public partial class SharedViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _count;
+
+    [RelayCommand]
+    public void Increment()
+    {
+        Count++;
+    }
+}
---
+++ MVVM/MVVM.Lib/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MVVM.Lib.Shared;
+
+namespace MVVM.Lib;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        DataContext = new SharedViewModel();
+        InitializeComponent();
+    }
+}
